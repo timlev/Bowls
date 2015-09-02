60d9882c5791e83e7bc2831f6ac44ce67e84f99a
Tim Leverentz <timlev@gmail.com>
2015-09-01 19:10:56 -0500

    full screen lock button

--- headsupdisplay.js
+++ headsupdisplay.js
@@ -1,3 +1,8 @@
 screen.orientation.addEventListener("change", function(e) {
       alert(screen.orientation.type + " " + screen.orientation.angle);
     }, false);
+document.getElementById("button").addEventListener("click", function() {
+  document.documentElement.requestFullScreen();
+  screen.orientation.lock("landscape-primary");
+
+}, false);

c31555cf4ae17f88c0854c5a21e584c8bd5dccff
Tim Leverentz <timlev@gmail.com>
2015-09-01 19:12:20 -0500

    Added onload function

--- headsupdisplay.js
+++ headsupdisplay.js
@@ -1,8 +1,11 @@
 screen.orientation.addEventListener("change", function(e) {
       alert(screen.orientation.type + " " + screen.orientation.angle);
     }, false);
+
+function onload(){
 document.getElementById("button").addEventListener("click", function() {
   document.documentElement.requestFullScreen();
   screen.orientation.lock("landscape-primary");
 
 }, false);
+}

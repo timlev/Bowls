e580a9648b6a48431ec88818eae153a6019545dd
Tim Leverentz <timlev@gmail.com>
2015-09-15 21:36:32 -0500

    actually updated headsupdisplay.js

--- headsupdisplay.js
+++ headsupdisplay.js
@@ -5,8 +5,20 @@ screen.orientation.addEventListener("change", function(e) {
 function onload(){
   if (window.DeviceMotionEvent) {
     alert("DeviceMotionEvent supported");
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
   }
 document.getElementById("button").addEventListener("click", function() {
   screen.orientation.lock("landscape-primary");
 }, false);
+}
+
+
+function deviceMotionHandler(eventData){
+  var info, xyz = "[X,Y,Z]";
+  //Grab Acceleration Data from Result
+  var acceleration = eventData.acceleration;
+  info = xyz.replace("X", acceleration.x);
+  info = info.replace("Y", acceleration.y);
+  info = info.replace("Z", acceleration.z);
+  document.getElementById("accel").innerHTML = info;
 }

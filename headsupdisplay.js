20aaf9b96403bc8852d7d8c636524e19cb0569f2
Tim Leverentz <timlev@gmail.com>
2015-09-16 19:14:39 -0500

    moved handler back into onload

--- headsupdisplay.js
+++ headsupdisplay.js
@@ -1,20 +1,22 @@
-function deviceMotionHandler(eventData){
-  var info, xyz = "[X,Y,Z]";
-  //Grab rotation Data from Result
-  var rotation = eventData.rotationRate;
-  info = xyz.replace("X", rotation.alpha);
-  info = info.replace("Y", rotation.beta);
-  info = info.replace("Z", rotation.gamma);
-  document.getElementById("accel").innerHTML = info;
-};
+
 
 function onload(){
   // screen.orientation.addEventListener("change", function(e) {
   //       alert(screen.orientation.type + " " + screen.orientation.angle);
   //     }, false);
-  if (window.DeviceMotionEvent) {
+
+  function deviceMotionHandler(eventData){
+    var info, xyz = "[X,Y,Z]";
+    //Grab rotation Data from Result
+    var rotation = eventData.rotationRate;
+    info = xyz.replace("X", rotation.alpha);
+    info = info.replace("Y", rotation.beta);
+    info = info.replace("Z", rotation.gamma);
+    document.getElementById("accel").innerHTML = info;
+  };
+  if (screen.DeviceMotionEvent) {
     alert("DeviceMotionEvent supported");
-    window.addEventListener('devicemotion', deviceMotionHandler, false);
+    screen.addEventListener('devicemotion', deviceMotionHandler, false);
   };
 
   document.getElementById("button").addEventListener("click", function() {
